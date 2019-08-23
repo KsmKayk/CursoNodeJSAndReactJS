8cbf16c5e4a9ba7a5291c343da8537dcd980e626
Kayk <ksm.kayk@outlook.com>
2019-08-23 14:46:45 -0300

    trocando de postgres para mysql, configurando pagina de signup, (erro em, avatar img(não aparece)) prosseguindo para enviar ao mysql

--- Node/modulo 2/src/config/database.js
+++ Node/modulo 2/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-	dialect: "postgres",
+	dialect: "mysql",
 	host: "127.0.0.1",
-	username: "docker",
-	password: "docker",
-	database: "nodemodulo2",
+	username: "root",
+	password: "root",
+	database: "gonodem2",
 	operatorAliases: false,
 	define: {
 		timestamps: true,
